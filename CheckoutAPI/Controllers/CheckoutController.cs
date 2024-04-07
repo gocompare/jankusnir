9e3dbeb61560bd8d234f88bf89c1fcea5d54a15b
Ján Kušnír <xkusnir1@fi.muni.cz>
2024-04-07 12:49:33 +0200

    feat: initial project setup with dummy endpoint

---
+++ CheckoutAPI/Controllers/CheckoutController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CheckoutAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CheckoutController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody] string value)
+        {
+            return Ok(0);
+        }
+    }
+}
