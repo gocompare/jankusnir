29d25c71c2376d89101fbaed2cb8f1ed10654017
Ján Kušnír <xkusnir1@fi.muni.cz>
2024-04-07 23:06:43 +0200

    feat: added a dummy implementation for IDataRepository

---
+++ CheckoutBL/DataRepository/DummyDataRepository.cs
@@ -0,0 +1,34 @@
+﻿using CheckoutBL.Models;
+using CheckoutBL.Offer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutBL.DataRepository
+{
+    public class DummyDataRepository : IDataRepository
+    {
+        public IEnumerable<IOffer> GetOffers()
+        {
+            var offers = new List<IOffer>
+            {
+                new MultiBuyOffer(new Product {SKU = "A", Price = 50}, 3, 130),
+                new MultiBuyOffer(new Product {SKU = "B", Price = 30}, 2, 45)
+            };
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<string, Product> GetProducts()
+        {
+            return new Dictionary<string, Product>
+            {
+                {"A", new Product {SKU = "A", Price = 50}},
+                {"B", new Product {SKU = "B", Price = 30}},
+                {"C", new Product {SKU = "C", Price = 20}},
+                {"D", new Product {SKU = "D", Price = 15}},
+            };
+        }
+    }
+}
