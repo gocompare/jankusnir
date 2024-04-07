6d4f82f4062a62422fe8ae5a771eeaab001c9af5
Ján Kušnír <xkusnir1@fi.muni.cz>
2024-04-07 23:06:12 +0200

    feat: added IDataRepository

---
+++ CheckoutBL/DataRepository/IDataRepository.cs
@@ -0,0 +1,16 @@
+﻿using CheckoutBL.Models;
+using CheckoutBL.Offer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutBL.DataRepository
+{
+    public interface IDataRepository
+    {
+        public IEnumerable<IOffer> GetOffers();
+        public IDictionary<string, Product> GetProducts();
+    }
+}
