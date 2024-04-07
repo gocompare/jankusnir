b126fb861cc35d4006e39340f351616082203593
Ján Kušnír <xkusnir1@fi.muni.cz>
2024-04-07 17:06:05 +0200

    feat: implemented MultiBuyOffer

---
+++ CheckoutBL/Internal/Offer/MultiBuyOffer.cs
@@ -0,0 +1,34 @@
+﻿using CheckoutBL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutBL.Internal.Offer
+{
+    internal class MultiBuyOffer : IOffer
+    {
+        private readonly Product product;
+        private readonly int requiredCount;
+        private readonly decimal newPrice;
+
+        public MultiBuyOffer(Product product, int requiredCount, decimal newPrice)
+        {
+            this.product = product;
+            this.requiredCount = requiredCount;
+            this.newPrice = newPrice;
+        }
+
+        public decimal ApplyOffer(Dictionary<string, int> shoppingCart, Dictionary<string, Product> productCatalog)
+        {
+            int itemsCount = shoppingCart[product.SKU];
+            decimal fullPrice = itemsCount * product.Price;
+
+            var offersApplied = itemsCount / requiredCount;
+            decimal discountedPrice = offersApplied * newPrice + (itemsCount - offersApplied * requiredCount);
+
+            return fullPrice = discountedPrice;
+        }
+    }
+}
