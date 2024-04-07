46f9731a6b9677a387545852da921e327291e6ff
Ján Kušnír <xkusnir1@fi.muni.cz>
2024-04-07 17:41:26 +0200

    feat: implemented NaiveDiscountCalculator

---
+++ CheckoutBL/Internal/DiscountCalculator/NaiveDiscountCalculator.cs
@@ -0,0 +1,24 @@
+﻿using CheckoutBL.Internal.Offer;
+using CheckoutBL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutBL.Internal.DiscountCalculator
+{
+    internal class NaiveDiscountCalculator : IDiscountCalculator
+    {
+        public decimal CalculateDiscount(Dictionary<string, int> shoppingCart, Dictionary<string, Product> productCatalog, List<IOffer> offers)
+        {
+            decimal discount = 0;
+            foreach (var offer in offers)
+            {
+                discount += offer.CalculateOfferDiscount(shoppingCart);
+            }
+
+            return discount;
+        }
+    }
+}
