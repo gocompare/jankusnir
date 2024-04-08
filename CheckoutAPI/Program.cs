711a0ab007b1c94e061b594e7fef3c1f398eb574
Ján Kušnír <xkusnir1@fi.muni.cz>
2024-04-08 09:45:05 +0200

    feat: register interface implementations in Program.cs

--- CheckoutAPI/Program.cs
+++ CheckoutAPI/Program.cs
@@ -1,3 +1,7 @@
+using CheckoutBL.DataRepository;
+using CheckoutBL.DiscountCalculator;
+using CheckoutBL.Services.CheckoutService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register impl. of IDataRepository
+builder.Services.AddSingleton<IDataRepository, DummyDataRepository>();
+
+// Register impl. of IDiscountCalculator
+builder.Services.AddSingleton<IDiscountCalculator, NaiveDiscountCalculator>();
+
+// Register impl. of ICheckoutService
+builder.Services.AddSingleton<ICheckoutService, CheckoutService>();
 
 var app = builder.Build();
 
