5e52f03c076b539bf3326c604f7ee82fe19765f5
Ján Kušnír <xkusnir1@fi.muni.cz>
2024-04-08 09:44:17 +0200

    feat: controller now using ICheckoutService

--- CheckoutAPI/Controllers/CheckoutController.cs
+++ CheckoutAPI/Controllers/CheckoutController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CheckoutBL.Services.CheckoutService;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,10 +9,18 @@ namespace CheckoutAPI.Controllers
     [ApiController]
     public class CheckoutController : ControllerBase
     {
+        private readonly ICheckoutService _checkoutService;
+
+        public CheckoutController(ICheckoutService checkoutService)
+        {
+            _checkoutService = checkoutService;
+        }
+
         [HttpPost]
         public IActionResult Post([FromBody] string value)
         {
-            return Ok(0);
+            var price = _checkoutService.CalculatePrice(value);
+            return Ok(price);
         }
     }
 }
